095b49fb751766b9109e94c688f56023c8857bff
github username <github email address>
2019-03-11 12:25:15 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -13,11 +13,31 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
+      #edit here. I want all the items to return and if not, it'll return empty
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+      end
     end
-
+  elsif req.path.match(/add/)
+        item_to_add = req.params["item"]
+        if @@items.include? item_to_add
+          @@cart << item_to_add
+          resp.write "added #{item_to_add}"
+        else
+          resp.write "We don't have that item!"
+        end
+      else
+        resp.write "Path Not Found"
+      end
     resp.finish
+  end
+
+  def all
+
   end
 
   def handle_search(search_term)
